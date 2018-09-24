b6eab32013019792e89a1266981a3dc0a1d4a62d
joohokim <ravenerkr@gmail.com>
2018-09-24 14:43:36 +0900

    카타

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 
 namespace Kata
@@ -689,6 +690,99 @@ namespace Factorial
                 Assert.AreEqual(17, Kata.СenturyFromYear(1601));
                 Assert.AreEqual(20, Kata.СenturyFromYear(2000));
             }
+        }
+
+        [Test]
+        [TestCase(new int[] { 2, 2, 2, 2 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 5, 10 }, ExpectedResult = 7)]
+        public static int FixedTest(int[] marks)
+        {
+            return GetAverage(marks);
+        }
+
+        [Test]
+        public void Test_ChromosomeCheck()
+        {
+            Assert.AreEqual("Congratulations! You're going to have a son.", ChromosomeCheck("XY"));
+            Assert.AreEqual("Congratulations! You're going to have a daughter.", ChromosomeCheck("XX"));
+
+            Assert.AreEqual(new string[] { "Robin", "Singh" }, "Robin Singh".Split(' '));
+        }
+
+        public static string ChromosomeCheck(string sperm)
+        {
+            switch (sperm)
+            {
+                case "XY":
+                    return "Congratulations! You're going to have a son.";
+
+                case "XX":
+                    return "Congratulations! You're going to have a daughter.";
+
+                default:
+                    throw new Exception("only xx or xy is valid");
+            }
+        }
+
+        [Test]
+        [TestCase(1, ExpectedResult = "1")]
+        [TestCase(2, ExpectedResult = "10")]
+        [TestCase(10, ExpectedResult = "1010101010")]
+        public static string FixedTest(int size)
+        {
+            return Stringy(size);
+        }
+
+        public static string Stringy(int size)
+        {
+            return new string(Enumerable.Range(1, size).Select(x => x % 2 == 0 ? '0' : '1').ToArray());
+        }
+
+        [Test]
+        public void GenericTests()
+        {
+            var haystack_1 = new object[] { '3', "123124234", null, "needle", "world", "hay", 2, '3', true, false };
+            var haystack_2 = new object[] { "283497238987234", "a dog", "a cat", "some random junk", "a piece of hay", "needle", "something somebody lost a while ago" };
+            var haystack_3 = new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, "needle", 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54 };
+
+            Assert.AreEqual("found the needle at position 3", FindNeedle(haystack_1));
+            Assert.AreEqual("found the needle at position 5", FindNeedle(haystack_2));
+            Assert.AreEqual("found the needle at position 30", FindNeedle(haystack_3));
+        }
+
+        public static string FindNeedle(object[] haystack)
+        {
+            int foundIndex = Array.IndexOf(haystack, haystack.First(x => x as string == "needle"));
+            return $"found the needle at position {foundIndex}";
+        }
+
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(new int[] { 1 }, distinct(new int[] { 1 }));
+            Assert.AreEqual(new int[] { 1, 2 }, distinct(new int[] { 1, 2 }));
+            Assert.AreEqual(new int[] { 1, 2 }, distinct(new int[] { 1, 1, 2 }));
+        }
+
+        public static int[] distinct(int[] a)
+        {
+            return a.Distinct().ToArray();
+        }
+
+        [Test]
+        public void Test_ReverseSeq()
+        {
+            Assert.That(ReverseSeq(5), Is.EqualTo(new int[] { 5, 4, 3, 2, 1 }));
+        }
+
+        public static int[] ReverseSeq(int n)
+        {
+            return Enumerable.Range(1, n).Reverse().ToArray();
+        }
+
+        public static int GetAverage(int[] marks)
+        {
+            return (int)Math.Floor(marks.Average());
         }
 
         [TestFixture]
